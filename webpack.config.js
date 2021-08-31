973a4e3d9dea53fcb69aa061e5805f3a7dfcdbf7
Manvel Saroyan <saroyanm@gmail.com>
2021-08-31 14:26:03 +0300

    added, lit, webpack and http-server setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const argv = require("minimist")(process.argv.slice(2));
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "advanced-selecting.js",
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "smoke/index.html"}
+      ],
+    }),
+  ]
+};
+
+if (argv.watch)
+{
+  module.exports.watch = true;
+}
